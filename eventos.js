f73b4f82b6be34573430d0acc8f0f4e5407fa491
Josué Sosa <05josuesosa@gmail.com>
2025-08-13 00:10:27 -0300

    chore: dejar versión alternativa comentada del algoritmo como referencia

--- eventos.js
+++ eventos.js
@@ -1,5 +1,12 @@
 const div = document.getElementsByTagName('div')[0];
+const btn = document.getElementsByTagName('button')[0];
 
 div.addEventListener('click', function(){
     alert('Hola! Soy el div')
 });
+
+// Equivalente al atributo 'onclick' del botón:
+// btn.addEventListener('click', function(event)
+//     alert('Hola!');
+//     event.stopPropagation();
+// });
